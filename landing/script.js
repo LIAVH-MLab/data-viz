b80975105d025fbcbbb9dd0ba00bd54a48f83960
Can Sucuoğlu <cansucuoglu@gmail.com>
2025-03-12 14:15:08 -0400

    Created landing page

---
+++ landing/script.js
@@ -0,0 +1,64 @@
+
+
+const links = {
+    'dancing-girl': '/',
+    'wall': '/wall',
+    'section': '/section',
+    'well': '/well',
+    'vessel': '/final-viz',
+
+}
+
+// create an event listener for image on hover
+document.querySelectorAll('image').forEach(img => {
+    img.addEventListener('mouseenter', function() {
+        let imgID = this.id;
+
+        // get both elements with the id imageID and swap classes show and hide
+        // remove show
+        // console.log( document.querySelector(`#${imgID}.show`));
+
+        let imageShow = document.querySelector(`#${imgID}.show`);
+        let imageHide = document.querySelector(`#${imgID}.hide`);
+
+
+        imageShow.classList.remove('show');
+        imageShow.classList.add('hide');
+
+        // add show
+        imageHide.classList.remove('hide');
+        imageHide.classList.add('show');
+
+        console.log(imageShow);
+        console.log(imageHide);
+        console.log(' ------- ' );
+       
+    });
+
+    img.addEventListener('mouseleave', function() {
+        let imgID = this.id;
+
+        // get both elements with the id imageID and swap classes show and hide
+        // remove show
+        // console.log( document.querySelector(`#${imgID}.show`));
+
+        let imageShow = document.querySelector(`#${imgID}.show`);
+        let imageHide = document.querySelector(`#${imgID}.hide`);
+
+        imageShow.classList.remove('show');
+        imageShow.classList.add('hide');
+
+        // add show
+        imageHide.classList.remove('hide');
+        imageHide.classList.add('show');
+    });
+
+
+    img.addEventListener('click', function() {
+        let imgID = this.id;
+        window.open(links[imgID], '_blank');    
+    });
+
+
+
+});
