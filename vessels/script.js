c2d9a602a8cc62f5d71b7c9c5b3496a60a9498cd
Can Sucuoğlu <cansucuoglu@gmail.com>
2025-03-25 22:33:41 -0400

    Added images and some padding etc

--- vessels/script.js
+++ vessels/script.js
@@ -12,6 +12,7 @@ d3.csv("./assets/data/mjd-vessels.csv", function(error, data) {
         let item = document.createElement("div");
         let idName = element['Column 3'].toLowerCase().replace(/\s+/g, '-');
         item.id = `vessel-${idName}`;
+        item.className = "vessel-item";
         container.node().appendChild(item);
         
         // add an h1 for the vessel name
@@ -21,21 +22,30 @@ d3.csv("./assets/data/mjd-vessels.csv", function(error, data) {
 
         // created an emoty div, class vessel-image
         let vesselImage = document.createElement("div");
+        
         vesselImage.className = "vessel-image";
         item.appendChild(vesselImage);
         
         // add an iframe for the vessel image, using the link from the csv
         let frameLink = `${element['Link']}/embed`;
-        console.log("Frame link", frameLink);
+        // console.log("Frame link", frameLink);
 
         let iframe = document.createElement("iframe");
         iframe.src = frameLink;
-        iframe.width = "100%";
-        iframe.height = "150px";
-        // iframe.frameBorder = "0";
         iframe.allow = "autoplay; encrypted-media";
 
-        item.appendChild(iframe);
+
+        if (idName === 'pot-2') {
+            console.log("pot-2 is here")
+            let image = document.createElement("img");
+            image.src = "./assets/images/pot-2.jpg";
+            image.style.width = "100%";
+            image.style.height = "100%";
+
+            vesselImage.appendChild(image);
+        }
+
+        vesselImage.appendChild(iframe);
 
     });
 
